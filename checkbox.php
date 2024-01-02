6f867ffb9e58a91a81b9d7e91863c1f59a9417a7
TECHSYSPROGRAM <techsysprogram@gmail.com>
2024-01-02 19:45:52 +0100

    model01

---
+++ checkbox.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form checkbox 01</title>
+    <link rel="stylesheet" type="text/css" href="https://test.techsysprogram.com/css/checkbox.css">
+    
+</head>
+
+<body>
+    <div class="container">
+        <div class="form-check">
+            <input type="checkbox" id="cgv" name="cgv">
+            <label for="cgv">
+                J'accepte les conditions générales de vente <br>
+                aqui le lien de confidelité 
+            </label>
+        </div>
+    </div>
+</body>
+
+</html>
