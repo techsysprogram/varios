fe36952534edb2f4e714af6b0ad252b70f29ba9b
TECHSYSPROGRAM <techsysprogram@gmail.com>
2024-01-04 16:42:14 +0100

    primero44

---
+++ profil_card3.php
@@ -0,0 +1,41 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>profil_card3</title>
+    <link rel="stylesheet" type="text/css" href="css/profil_card3.css">
+</head>
+
+<body>
+    <div class="card-profile">
+    <div class="card-header-profile">
+        <div class="profile-image"></div>
+
+        <div class="profile-info">
+        <h3 class="profile-name">Jane Doe</h3>
+        <p class="profile-desc">Frontend Developer</p>
+        </div>
+    </div>
+    <div class="profile-card-body">
+        <ul class="status">
+        <li>
+            <span class="status-value">532</span>
+            <span class="status-text">Posts</span>
+        </li>
+        <li>
+            <span class="status-value">22M</span>
+            <span class="status-text">Followers</span>
+        </li>
+        <li>
+            <span class="status-value">442</span>
+            <span class="status-text">Following</span>
+        </li>
+        </ul>
+
+        <div class="action">
+        <button class="btn btn-success">Follow</button>
+        <button class="btn btn-outline">Message</button>
+        </div>
+    </div>
+    </div>
+</body>
